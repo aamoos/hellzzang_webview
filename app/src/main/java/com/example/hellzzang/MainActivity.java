e2bf8392033abb2e41f52fdcfa8132971679d982
aamoos <aamoos@naver.com>
2023-04-28 14:20:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hellzzang/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.hellzzang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView; // 웹뷰 선언
+    private WebSettings webSettings; //웹뷰세팅
+    private String url = "http://192.168.0.38:8080/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webSettings.setDomStorageEnabled(true);
+        webView.loadUrl(url);
+    }
+
+}
